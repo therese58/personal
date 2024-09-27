18afeccb6ab62c87c9b3ede62e44ef05c42057c1
therese58 <therese.luce9@gmail.com>
2024-09-27 16:06:07 +0900

    index 오류 수정 및 js 업데이트

---
+++ data/navi.js
@@ -0,0 +1,21 @@
+const navidata = [
+    {
+        d1text : "병원소개",
+        d1href : "http://www.naver.com",
+        __target :"_blank"
+      },
+    {
+        d1text : "맞춤한약",
+        d1href : "https://www.daum.net/",
+        __target : "_blank"},
+    {
+      d1text : "사암침법",
+      d1href : "https://www.daum.net/",
+      __target: "_blank"
+    },
+    {
+        d1text : "고객센터",
+        d1href : "#service",
+        __target :"_blank"
+    }
+  ]
